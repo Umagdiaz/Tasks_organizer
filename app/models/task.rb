a3802b1b5fadaa90f22c7bafe23a389d7b2ac05f
uma <umabaligranado@gmail.com>
2023-06-03 14:38:19 -0300

    validates

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,15 @@
 class Task < ApplicationRecord
   belongs_to :category
+
+  validates :name, :description, presence: true
+  validates :name, uniqueness: { case_insensitive: false }
+  validate :due_date_validay
+
+  def due_date_validay
+    return if due_date.blank?
+    return if due_date >= Date.today
+    errors.add :due_date
+  end
+
+
 end
