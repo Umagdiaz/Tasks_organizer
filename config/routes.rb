9c5d3f49b1c05377a044a61c478174f77ffc712f
uma <umabaligranado@gmail.com>
2023-06-04 22:57:20 -0300

    notes 1/2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :tasks
+  resources :tasks do 
+    resources :notes, only: [:create], controller: 'tasks/notes'
+  end 
   resources :categories
+
   root 'tasks#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
